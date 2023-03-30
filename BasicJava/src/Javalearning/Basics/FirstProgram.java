930457e2c6685ddb0c6f2dedc070d73346d79e0c
hp <hp@Rakesh-PC>
2023-03-30 20:29:50 +0530

    First Commit

---
+++ BasicJava/src/Javalearning/Basics/FirstProgram.java
@@ -0,0 +1,10 @@
+package Javalearning.Basics;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hi this is rakesh ");
+	}
+
+}
